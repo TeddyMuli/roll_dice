652ababc46843a530e541d35ffcae0af0cd4f53a
TeddyMuli <tmuli974@gmail.com>
2025-02-24 14:19:11 +0300

    initialize state

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ import random
 import pandas as pd
 import plotly.express as px
 
+if 'rolls' not in st.session_state:
+    st.session_state.rolls = []
+if 'roll_count' not in st.session_state:
+    st.session_state.roll_count = 0
+
 def roll_dice():
     result = random.randint(1, 6)
     return result
@@ -14,3 +19,4 @@ st.title('🎲 Rolling Dice Game')
 if st.button('Roll Dice'):
     result = roll_dice()
     st.header(f'You rolled a {result}')
+
