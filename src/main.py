f27a10fa6662b44364f8781717f653db3ebd63e2
TeddyMuli <tmuli974@gmail.com>
2025-02-24 14:45:52 +0300

    add pandas dataframe

--- src/main.py
+++ src/main.py
@@ -22,3 +22,25 @@ if st.button('Roll Dice'):
     result = roll_dice()
     st.header(f'You rolled a {result}')
 
+if st.session_state.roll_count > 0:
+    roll_data = pd.DataFrame(st.session_state.rolls, columns=['Result'])
+    stats = roll_data['Result'].value_counts().reset_index()
+    stats.columns['Number', 'Count']
+    stats = stats.sort_values('Number')
+    stats['Percentage'] = (stats['Count'] / len(st.session_state.rolls) * 100).round(2)
+
+    st.subheader('Roll Statistics')
+    st.dataframe(stats)
+
+    bar_chart = px.bar(stats,
+                       x='Number',
+                       y='Count',
+                       title='Distribution of Dice Rolls',
+                       labels={'Number': 'Dice Number', 'Count': 'Number of Rolls'},
+                       text=stats['Percentage'].apply(lambda x: f'{x}%'))
+    
+    bar_chart.update_traces(textposition='outside')
+    st.plotly_chart(bar_chart)
+
+    st.markdown(f'**Total Rolls** {st.session_state.roll_count}')
+
