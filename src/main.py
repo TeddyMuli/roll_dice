83f3dedfbd36830927159e292bf7bf2fe8e5872f
TeddyMuli <tmuli974@gmail.com>
2025-02-24 14:17:26 +0300

    add button to trigger rolling dice

--- src/main.py
+++ src/main.py
@@ -8,3 +8,9 @@ import plotly.express as px
 def roll_dice():
     result = random.randint(1, 6)
     return result
+
+st.title('🎲 Rolling Dice Game')
+
+if st.button('Roll Dice'):
+    result = roll_dice()
+    st.header(f'You rolled a {result}')
